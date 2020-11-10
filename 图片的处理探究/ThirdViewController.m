d212665e8bc13608f5d7f60aa775472880ec70ac
xumingyang <xumingyang@greatyun.com>
2020-11-10 14:53:22 +0800

    加载网络动态图片

--- 图片的处理探究/ThirdViewController.m
+++ 图片的处理探究/ThirdViewController.m
@@ -43,7 +43,10 @@
     if (_animatedImage) {
         return _animatedImage;
     }
-    _animatedImage = [SDAnimatedImage imageNamed:@"少司命.gif"];
+//    _animatedImage = [SDAnimatedImage imageNamed:@"少司命.gif"];//加载本地图片gif的写法
+    //加载网络图片的写法
+    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605000729372&di=c45883a78737a80195eb87095a9edf06&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F6ddcca1745377c1355e23c56a91a8543509de15524d605-lBLXNz_fw658"] options:0 error:nil];
+    _animatedImage = [[SDAnimatedImage alloc] initWithData:data];
     return _animatedImage;
 }
 
